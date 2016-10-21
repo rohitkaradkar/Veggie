9b3c9990c61dd2f9eb1e9df6ab34beac145ad201
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-10-21 16:26:56 +0530

    android studio update, instant run RuntimeException fix

---
+++ core/src/main/java/com/greentopli/core/AppClass.java
@@ -0,0 +1,14 @@
+package com.greentopli.core;
+
+import android.app.Application;
+
+/**
+ * Created by rnztx on 21/10/16.
+ */
+
+public class AppClass extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
