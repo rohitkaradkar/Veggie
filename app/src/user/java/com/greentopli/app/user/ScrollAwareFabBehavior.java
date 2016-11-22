0fd26a01ae2a59bd20a10013b28609f8189b228d
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-11-22 15:48:07 +0530

    adding fab scrolling behaviour

---
+++ app/src/user/java/com/greentopli/app/user/ScrollAwareFabBehavior.java
@@ -0,0 +1,51 @@
+package com.greentopli.app.user;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by rnztx on 13/9/16.
+ */
+public class ScrollAwareFabBehavior extends FloatingActionButton.Behavior {
+	public ScrollAwareFabBehavior(Context context, AttributeSet attributeSet) {
+		super();
+	}
+
+	@Override
+	public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,FloatingActionButton child,
+	                                   View directTargetChild,View target, int nestedScrollAxes) {
+		// check for vertical scrolling
+		return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+				super.onStartNestedScroll(coordinatorLayout, child,directTargetChild, target, nestedScrollAxes);
+	}
+
+	@Override
+	public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+	                           View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+		super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+		if (dyConsumed>0 && child.getVisibility()==View.VISIBLE) // scrolling down
+			animate(child,false);
+		else if (dyConsumed < 0 && child.getVisibility()!=View.VISIBLE) // scrolling up
+			animate(child,true);
+	}
+
+	private void animate(final FloatingActionButton button, final boolean show){
+
+		button.animate()
+				.translationY(show?1:button.getHeight())
+				.alpha(1.0f)
+				.setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						super.onAnimationEnd(animation);
+						button.setVisibility(show?View.VISIBLE:View.GONE);
+					}
+				});
+	}
+}
